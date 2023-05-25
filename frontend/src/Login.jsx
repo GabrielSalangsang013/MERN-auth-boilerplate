c42c66ffae44e9d20a9c429dd0b4c5a7b43d4053
GabrielSalangsang013 <gabrielsalangsang013@gmail.com>
2023-05-26 00:33:34 +0800

    Added Validation and Sanitizing in Login Page Front-End - v1.0.4

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,23 +1,66 @@
-import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { successLoginAction } from './actions/login';
 import { useNavigate } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { escape } from 'he';
+import * as Yup from 'yup';
 import DOMPurify from 'dompurify';  // FOR SANITIZING USER INPUT TO PREVENT XSS ATTACKS BEFORE SENDING TO THE BACKEND
 import axios from 'axios'
 axios.defaults.withCredentials = true
 
 const Login = () => {
     const navigate = useNavigate();
-    const isAuthenticated = useSelector((state) => state.isAuthenticated);
     const dispatch = useDispatch();
-    const [loginUsername, setLoginUsername] = useState('')
-    const [loginPassword, setLoginPassword] = useState('')
 
-    function handleLogin(e) {
-        e.preventDefault()
-        const sanitizedLoginUsername = DOMPurify.sanitize(loginUsername)
-        const sanitizedLoginPassword = DOMPurify.sanitize(loginPassword)
+    const initialValues = {
+        username: '',
+        password: ''
+    };
 
+    const validationSchema = Yup.object().shape({
+        username: Yup.string()
+            .required('Username is required')
+            .min(4, 'Username must be at least 4 characters')
+            .max(20, 'Username must not exceed 20 characters')
+            .matches(/^[a-zA-Z0-9_]+$/, 'Username can only contain letters, numbers, and underscores')
+            .test(
+                'username-security',
+                'Username should not contain sensitive information',
+                (value) => !/\b(admin|root|superuser)\b/i.test(value)
+            )
+            .test(
+                'username-sql-injection and username-xss',
+                'Invalid characters detected',
+                (value) => {
+                    const sanitizedValue = escape(value);
+                    return sanitizedValue === value; // Check if sanitized value is the same as the original value
+                }
+            ),
+        password: Yup.string()
+            .required('Password is required')
+            .min(12, 'Password must be at least 12 characters')
+            .matches(
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+                'Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character'
+            )
+            .test(
+                'password-security',
+                'Password should not be commonly used or easily guessable',
+                (value) => !/\b(password|123456789)\b/i.test(value)
+            )
+    });
+
+    const handleSubmit = (values) => {
+        // STEP 1: GET ALL THE INPUT VALUES THAT HAS BEEN SUCCESSFULLY PASSED TO VALIDATION
+        const {username, password} = values;
+        // END GET ALL THE INPUT VALUES THAT HAS BEEN SUCCESSFULLY PASSED TO VALIDATION
+        
+        // STEP 2: SANITIZE THE USER INPUT TO PREVENT XSS ATTACK
+        const sanitizedLoginUsername = DOMPurify.sanitize(username)
+        const sanitizedLoginPassword = DOMPurify.sanitize(password)
+        // END SANITIZE THE USER INPUT TO PREVENT XSS ATTACK
+
+        // STEP 3: SEND THE SANITIZED INPUT TO THE BACKEND FOR THE LOGIN PURPOSES
         axios.post('http://localhost:4000/api/v1/authentication/login', {
             username: sanitizedLoginUsername,
             password: sanitizedLoginPassword
@@ -38,16 +81,29 @@ const Login = () => {
                 alert(error.response.data.error)
             }
         })
+        // END SEND THE SANITIZED INPUT TO THE BACKEND FOR THE LOGIN PURPOSES
     }
 
     return (
-        <>
-            <h2>Login Form</h2>
-            <input type='text' placeholder='Enter username' onChange={e => setLoginUsername(e.target.value)} />
-            <input type='password' placeholder='Enter password' onChange={e => setLoginPassword(e.target.value)} />
-            <button type='submit' onClick={handleLogin}>Login</button>
-            {isAuthenticated ? <h3>You are logged in</h3> : <h3>You are not logged in</h3>}
-        </>
+        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
+            <Form>
+                <h1>Login Form</h1>
+                <div>
+                    <label htmlFor="username">Username: </label>
+                    <Field type="text" id="username" name="username" />
+                    <ErrorMessage name="username" component="div" />
+                </div>
+                
+                <div>
+                    <label htmlFor="password">Password: </label>
+                    <Field type="password" id="password" name="password" />
+                    <ErrorMessage name="password" component="div" />
+                </div>
+
+                <br/>
+                <button type="submit">Login</button>
+            </Form>
+        </Formik>
     )
 }
 
