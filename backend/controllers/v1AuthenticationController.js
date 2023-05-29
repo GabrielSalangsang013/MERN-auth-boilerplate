9f90bd5d384968c16f8d5ba6edd929589d2497f6
GabrielSalangsang013 <gabrielsalangsang013@gmail.com>
2023-05-29 22:13:19 +0800

    Rename sendEmail utility in the backend - v1.0.21

--- backend/controllers/v1AuthenticationController.js
+++ backend/controllers/v1AuthenticationController.js
@@ -7,7 +7,7 @@ const { escape } = require('he');
 const xss = require('xss'); // FOR XSS PROTECTION IN REGISTER AND LOGIN PURPOSES
 const mongoSanitize = require('express-mongo-sanitize'); // FOR NOSQL INJECTION PROTECTION IN REGISTER AND LOGIN PURPOSES
 const frontendConfig = require('../config/frontend');
-const sendEmail = require("../utils/sendEmail");
+const sendEmailActivationAccount = require("../utils/sendEmailActivationAccount");
 const ACCESS_TOKEN_EXPIRATION = 60 * 1000;
 
 const user = async (req, res) => {    
@@ -206,7 +206,7 @@ const register = async (req, res) => {
         `;
 
         try {
-            await sendEmail({
+            await sendEmailActivationAccount({
                 to: email,
                 subject: "MERN with Auth - Account Activation",
                 text: "Your account will be activated by clicking the link below",
---
+++ backend/utils/sendEmailActivationAccount.js
@@ -0,0 +1,35 @@
+const nodemailer = require("nodemailer");
+
+const sendEmailActivationAccount = async ({ to, subject, text, html }) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: process.env.NODE_ENV == "PRODUCTION" ? true : false,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+  const emailOptions = {
+    from: `MERN <${process.env.EMAIL_FROM}>`,
+    to,
+    subject,
+    text,
+    html,
+  };
+
+  // Sending email activation account
+  transporter.sendMail(emailOptions, (error, info) => {
+    if (error) {
+      console.log({
+        fileName: 'sendEmailActivationAccount.js',
+        errorDescription: 'There is something problem on the sending the activation link to the user via email.',
+        errorLocation: 'sendEmailActivationAccount',
+        error: error
+      });
+    }
+  });
+};
+
+module.exports = sendEmailActivationAccount;
